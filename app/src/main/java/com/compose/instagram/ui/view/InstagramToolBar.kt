4d389244d292730632cd9d55c8fcbd005a204a2e
Diego de O. Silva <diego.rof@hotmail.com>
2023-07-26 16:39:10 -0300

    Episódio 3 - Conclusão Toolbar

--- app/src/main/java/com/compose/instagram/ui/view/InstagramToolBar.kt
+++ app/src/main/java/com/compose/instagram/ui/view/InstagramToolBar.kt
@@ -1,11 +1,13 @@
 package com.compose.instagram.ui.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,39 +15,44 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.compose.instagram.R
 
 @Composable
 fun InstagramToolBar() {
-    Row(
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .height(56.dp)
-            .fillMaxWidth()
-    ) {
-
-        Text(text = "Instagram", fontSize = 32.sp, modifier = Modifier.weight(1f))
-
-        Image(
-            painter = painterResource(id = R.drawable.ic_notification),
+    Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+        Row(
             modifier = Modifier
-                .size(32.dp)
-                .padding(end = 8.dp)
-                .align(Alignment.CenterVertically),
-            contentDescription = "Ícone de notificações na barra de ferramentas."
-        )
+                .padding(horizontal = 16.dp)
+                .height(56.dp)
+        ) {
 
-        Image(
-            painter = painterResource(id = R.drawable.ic_message),
-            modifier = Modifier
-                .size(32.dp)
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically),
-            contentDescription = "Ícone de mensagens diretas (DM) na barra de ferramentas."
-        )
+            Text(
+                text = "Instagram",
+                modifier = Modifier.weight(1f),
+                style = MaterialTheme.typography.headlineLarge
+            )
 
+            Image(
+                painter = painterResource(id = R.drawable.ic_notification),
+                modifier = Modifier
+                    .size(32.dp)
+                    .padding(end = 8.dp)
+                    .align(Alignment.CenterVertically),
+                contentDescription = "Ícone de notificações na barra de ferramentas."
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_message),
+                modifier = Modifier
+                    .size(32.dp)
+                    .padding(start = 8.dp)
+                    .align(Alignment.CenterVertically),
+                contentDescription = "Ícone de mensagens diretas (DM) na barra de ferramentas."
+            )
+
+        }
     }
+
 }
 
 @Preview(showBackground = true)
