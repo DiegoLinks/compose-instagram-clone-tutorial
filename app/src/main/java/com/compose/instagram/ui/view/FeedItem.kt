371ff05b0639da372f8cb13ce5b9fb864ab2a294
Diego de O. Silva <diego.rof@hotmail.com>
2023-08-01 17:54:04 -0300

    Episódio 08 - Continuação do Feed

--- app/src/main/java/com/compose/instagram/ui/view/FeedItem.kt
+++ app/src/main/java/com/compose/instagram/ui/view/FeedItem.kt
@@ -1,11 +1,96 @@
 package com.compose.instagram.ui.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.compose.instagram.R
 import com.compose.instagram.data.model.Feed
+import com.compose.instagram.ui.theme.spacingLarge
+import com.compose.instagram.ui.theme.spacingMedium
+import com.compose.instagram.ui.theme.spacingSmall
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun FeedItem(feed: Feed) {
+
+    val userAvatarContentDesc = stringResource(R.string.content_description_feed_avatar)
+    val feedImageContentDesc = stringResource(R.string.content_description_feed_image)
+
+    Column(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+
+        Row(
+            modifier = Modifier
+                .padding(horizontal = spacingSmall)
+                .padding(top = spacingLarge)
+        ) {
+
+            GlideImage(
+                model = feed.userAvatar,
+                contentDescription = userAvatarContentDesc,
+                modifier = Modifier
+                    .size(36.dp)
+                    .fillMaxSize()
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+
+            Column {
+                Text(
+                    text = feed.userNickName,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = spacingMedium),
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    textAlign = TextAlign.Start
+                )
+
+                Text(
+                    text = feed.localName,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = spacingMedium),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    textAlign = TextAlign.Start
+                )
+            }
+
+        }
+
+        GlideImage(
+            model = feed.imageUrl,
+            contentDescription = feedImageContentDesc,
+            modifier = Modifier
+                .padding(top = spacingLarge)
+                .height(256.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+
+    }
+
 }
 
 @Preview(showBackground = true)
